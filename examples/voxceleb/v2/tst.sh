81473351d3173fc58041087248aa2728e080a123
mscer <jiaqi.zjq@hotmail.com>
2024-10-27 11:49:44 +0800

    update file

---
+++ examples/voxceleb/v2/tst.sh
@@ -0,0 +1,8 @@
+bash run.sh --stage 1 --stop_stage 1
+bash run.sh --stage 2 --stop_stage 2
+bash run.sh --stage 3 --stop_stage 3
+bash run.sh --stage 4 --stop_stage 4
+bash run.sh --stage 5 --stop_stage 5
+bash run.sh --stage 6 --stop_stage 6
+bash run.sh --stage 7 --stop_stage 7
+bash run.sh --stage 8 --stop_stage 8
